fce607cc105708593abf0d343fd4cc7bc97bca29
chuanxiao.li <674913892@qq.com>
2018-08-28 17:49:12 +0800

    添加相关数据库表

--- release/models.py
+++ release/models.py
@@ -9,6 +9,10 @@ class Relese(models.Model):
     deploy: 标识该次发布部署的环境
     deploy_time：标识该次部署具体时间
     """
+    TEST = 'TEST'
+    YL = 'YL'
+    HD = 'HD'
+    SC = 'SC'
     DEPLOY_ENV_CHOICES = (
     (TEST,"测试环境"),
     (YL,"演练环境"),
@@ -18,5 +22,31 @@ class Relese(models.Model):
 
     iterative_version = models.CharField(max_length=8, verbose_name="迭代版本")
     release_title = models.CharField(max_length=60, verbose_name="上线标题")
-    deploy_evn = models.CharField(max_length=5, choices=DEPLOY_ENV_CHOICES, default=YL)
+    deploy_evn = models.CharField(max_length=4, choices=DEPLOY_ENV_CHOICES, default=YL)
+    deploy_time = models.DateTimeField(auto_now=True)
+    release_func = models.TextField()
     
+
+
+    def __str__(self):
+        return self.release_title
+
+class GroupInfo(models.Model):
+    """
+    项目组信息
+    """
+    group_name = models.CharField(max_length=30, verbose_name="组名", unique=True)
+    group_url = models.URLField(null=True, blank=True)
+    description = models.CharField(max_length=200, verbose_name='组描述', null=True, blank=True)
+
+    def __str__(self):
+        return self.group_name
+    
+    
+
+class ProjectInfo(models.Model):
+    """
+    项目表，主要记录每个项目的详细信息
+    """
+    project_name = models.CharField(max_length=30, verbose_name="项目名称")
+    group = models.ForeignKey(GroupInfo, to_field='group_name', on_delete=models.PROTECT)
